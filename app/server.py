0eedc64a98fe04fc39c95b97aca2b38896f37357
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-13 10:00:49 +0000

    return 'other', if input image isn't in the range

--- app/server.py
+++ app/server.py
@@ -52,8 +52,11 @@ async def analyze(request):
     data = await request.form()
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    prediction, _, outputs = learn.predict(img)
+    if outputs[0].item() < 0.9 and outputs[1].item() < 0.9:
+        return JSONResponse({'result': 'other'})
+    else:
+        return JSONResponse({'result': str(prediction)})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8080)
